1f4acaafaa79637528bdcaf273db6d797dac8135
fy <yfeng880@163.com>
2022-12-16 11:25:25 +0800

    first commit

---
+++ hellolib/src/hello.cpp
@@ -0,0 +1,8 @@
+#include <hello/hello.h>
+
+namespace hello {
+    void printHello()
+    {
+        std::cout<<"fy-> hello, world"<<std::endl;
+    }
+}
---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file main.cpp
+ * @author fy (yfeng880@163.com)
+ * @brief  cmake_test
+ * @version 1.0
+ * @date 2022-12-16
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+#include <hello/hello.h>
+
+using namespace std;
+
+/**
+ * @brief Main Function tset
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+int main(int argc, char* argv[]){
+    
+    hello::printHello();
+    
+    return 0;
+}
